d52f52eb45bc2f5a82a335fde8f8a4c19915f672
Karol Patecki <karol.patecki09@gmail.com>
2018-01-07 18:51:10 +0100

    Changed way of connection to db

--- src/pl/carWorkshop/DbUtil.java
+++ src/pl/carWorkshop/DbUtil.java
@@ -12,41 +12,41 @@ import javax.sql.DataSource;
 public class DbUtil {
 	
 	
-	private static DataSource ds;
-	@SuppressWarnings("unused")
-	public static Connection getConn () throws SQLException{
-		
-		return getInstance().getConnection();
-	}
-	
-	private static DataSource getInstance() {
-		
-		if (ds==null) {
-			try {
-				Context ctx = new InitialContext();
-				ds= (DataSource)ctx.lookup("java:comp/env/jdbc/crm");
-			}catch (NamingException e) {
-				e.printStackTrace();
-			}
-		}
-		return ds;
-	}
+//	private static DataSource ds;
+//	@SuppressWarnings("unused")
+//	public static Connection getConn () throws SQLException{
+//		
+//		return getInstance().getConnection();
+//	}
+//	
+//	private static DataSource getInstance() {
+//		
+//		if (ds==null) {
+//			try {
+//				Context ctx = new InitialContext();
+//				ds= (DataSource)ctx.lookup("java:comp/env/crm");
+//			}catch (NamingException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//		return ds;
+//	}
 	
 
-//	private static String DB_URL = "jdbc:mysql://localhost:3306/carWorkshop?useSSL=false";
-//	private static String DB_USRNM = "root";
-//	private static String DB_PSSWRD = "coderslab";
-//	
-//	public static Connection connectToDb () throws SQLException {
-//		
-//
-//		try {
-//			Class.forName("com.mysql.jdbc.Driver").newInstance();
-//		}catch (Exception e) {
-//			
-//		}
-//		
-//		return DriverManager.getConnection(DB_URL, DB_USRNM, DB_PSSWRD);
-//		
-//	}
+	private static String DB_URL = "jdbc:mysql://localhost:3306/crm?useSSL=false";
+	private static String DB_USRNM = "root";
+	private static String DB_PSSWRD = "coderslab";
+	
+	public static Connection getConn () throws SQLException {
+		
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		}catch (Exception e) {
+			
+		}
+		
+		return DriverManager.getConnection(DB_URL, DB_USRNM, DB_PSSWRD);
+		
+	}
 }
